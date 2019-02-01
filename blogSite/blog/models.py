d118a0a40d3144468eb6f93ccf90a2b8fb11920d
bjaus <brandon.jaus@gmail.com>
2019-02-01 16:53:57 -0600

    model created and migrated

--- blogSite/blog/models.py
+++ blogSite/blog/models.py
@@ -1,3 +1,68 @@
-from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django_extensions.db.fields import AutoSlugField
+from django.db.models import (
+    CASCADE,
+    CharField,
+    DateTimeField,
+    ForeignKey,
+    Manager,
+    Model,
+#    SlugField,
+    TextField,
+)
 
-# Create your models here.
+class PublishedManager(Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
+
+
+class Post(Model):
+
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published')
+    )
+
+    # Mode Fields
+    title = CharField(
+        max_length=250
+    )
+    slug = AutoSlugField(
+        max_length=250,
+        populate_from='title',
+        unique_for_date='publish',
+    )
+    author = ForeignKey(
+        User,
+        related_name='blog_posts',
+        on_delete=CASCADE
+    )
+    body = TextField()
+    publish = DateTimeField(
+        default=timezone.now
+    )
+    created = DateTimeField(
+        auto_now_add=True
+    )
+    updated = DateTimeField(
+        auto_now=True
+    )
+    status = CharField(
+        max_length=10,
+        choices=STATUS_CHOICES,
+        default='draft'
+    )
+
+    # Model Managers
+    objects = Manager()
+    published = PublishedManager()
+
+    class Meta:
+        ordering = [
+            '-publish',
+        ]
+
+    def __str__(self):
+        return self.title
+
