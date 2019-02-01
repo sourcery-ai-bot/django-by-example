10d76e42964030654d0626ee15824282ccb1b1b5
bjaus <brandon.jaus@gmail.com>
2019-02-01 16:54:26 -0600

    blog admin view created and registered

--- blogSite/blog/admin.py
+++ blogSite/blog/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'slug',
+        'author',
+        'publish',
+        'status',
+    )
+    list_filter = (
+        'status',
+        'created',
+        'publish',
+        'author',
+    )
+    search_fields = (
+        'title',
+        'body',
+    )
+    raw_id_fields = ('author', )
+    date_hierarchy = 'publish'
+    ordering = [
+        'status',
+        '-publish',
+    ]
+
+admin.site.register(Post, PostAdmin)
+
