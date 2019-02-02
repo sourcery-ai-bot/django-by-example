e86db272cfd89f69cee86f9393357743356f397d
bjaus <brandon.jaus@gmail.com>
2019-02-02 15:48:50 -0600

    workings with tags

--- blogSite/blog/views.py
+++ blogSite/blog/views.py
@@ -2,40 +2,50 @@ from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm
 
 from django.core.mail import send_mail
+from django.db.models import Count
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView
+#from django.views.generic import ListView
 from django.core.paginator import (
     Paginator,
     EmptyPage,
     PageNotAnInteger,
 )
+from taggit.models import Tag
+
 
 # Class Based View
-class PostListView(ListView):
-    queryset = Post.published.all()
-    context_object_name = 'posts'
-    paginate_by = 4
-    template_name = 'blog/post/list.html'
+#class PostListView(ListView):
+#    queryset = Post.published.all()
+#    context_object_name = 'posts'
+#    paginate_by = 4
+#    template_name = 'blog/post/list.html'
 
 # Function Based View
-#def post_list(request):
-#    object_list = Post.published.all()
-#    paginator = Paginator(object_list, 5) # 3 posts on each page
-#    page = request.GET.get('page')
-#    try:
-#        posts = paginator.page(page)
-#    except PageNotAnInteger:
-#        # If page is not an integer deliver the first page
-#        posts = paginator.page(1)
-#    except EmptyPage:
-#        # If page is out of range deliever last page of results
-#        posts = paginator.page(paginator.num_pages)
-#    return render(request,
-#                  'blog/post/list.html',
-#                  {
-#                      'page': page,
-#                      'posts': posts,
-#                  })
+def post_list(request, tag_slug=None):
+    object_list = Post.published.all()
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+
+    paginator = Paginator(object_list, 3)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range deliever last page of results
+        posts = paginator.page(paginator.num_pages)
+    return render(request,
+                  'blog/post/list.html',
+                  {
+                      'page': page,
+                      'posts': posts,
+                      'tag': tag
+                  })
 
 
 def post_detail(request, year, month, day, post):
@@ -62,6 +72,14 @@ def post_detail(request, year, month, day, post):
             new_comment.save()
     else:
         comment_form = CommentForm()
+
+    # List of similiar posts
+    post_tags_id = post.tags.values_list('id', flat=True)
+    similiar_posts = Post.published.filter(tags__in=post_tags_id) \
+                                   .exclude(id=post.id)
+    similiar_posts = similiar_posts.annotate(same_tags=Count('tags')) \
+                                   .order_by('-same_tags', '-publish')[:4]
+
     return render(request,
                   'blog/post/detail.html',
                   {
@@ -69,6 +87,7 @@ def post_detail(request, year, month, day, post):
                       'comment_form': comment_form,
                       'new_comment': new_comment,
                       'post': post,
+                      'similiar_posts': similiar_posts,
                   })
 
 
