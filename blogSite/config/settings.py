91cee8d6b617e49faf80e2dced8f3750d372a44e
bjaus <brandon.jaus@gmail.com>
2019-02-02 17:14:06 -0600

    switch db to postgresql

--- blogSite/config/settings.py
+++ blogSite/config/settings.py
@@ -85,9 +85,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD')
+    },
+    'testing': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    },
 }
 
 
