72451b2678e0c2362da5d5c02bb8d3011294f986
bjaus <brandon.jaus@gmail.com>
2019-02-03 16:19:27 -0600

    added edit update messages

--- socialSite/account/views.py
+++ socialSite/account/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from .forms import LoginForm, UserRegistrationForm, \
@@ -70,6 +71,10 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request,
+                             'Profile Updated Successfully')
+        else:
+            messages.error(request, 'Error Updating Your Profile')
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
