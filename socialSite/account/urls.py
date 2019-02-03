59438265dbfca02847fbf88b5b1db9678f1d4aa2
bjaus <brandon.jaus@gmail.com>
2019-02-03 15:06:26 -0600

    added user registration url

--- socialSite/account/urls.py
+++ socialSite/account/urls.py
@@ -30,4 +30,5 @@ urlpatterns = [
          name='password_reset_complete'),
     # alternative way to include authentication views
     # path('', include('django.contrib.auth.urls')),
+    path('register/', views.register, name='register'),
 ]
