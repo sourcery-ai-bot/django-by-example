75a3be58d687c6b6957c904d3db13af6a82c63cd
bjaus <brandon.jaus@gmail.com>
2019-02-03 14:56:40 -0600

    added alt way to include auth views

--- socialSite/account/urls.py
+++ socialSite/account/urls.py
@@ -28,4 +28,6 @@ urlpatterns = [
     path('reset/done',
          auth_views.PasswordResetCompleteView.as_view(),
          name='password_reset_complete'),
+    # alternative way to include authentication views
+    # path('', include('django.contrib.auth.urls')),
 ]
