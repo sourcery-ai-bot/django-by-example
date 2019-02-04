61f4103e1f383f796738c6a55fb232a9dadc8d4a
bjaus <brandon.jaus@gmail.com>
2019-02-04 12:54:28 -0600

    added user follow url

--- socialSite/account/urls.py
+++ socialSite/account/urls.py
@@ -47,4 +47,5 @@ urlpatterns = [
     # users
     path("users/", views.user_list, name="user_list"),
     path("users/<username>/", views.user_detail, name="user_detail"),
+    path("users/follow/", views.user_follow, name="user_follow"),
 ]
