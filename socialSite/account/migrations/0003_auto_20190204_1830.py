bdc5dc6b655896334ba7894ddec974345115d439
bjaus <brandon.jaus@gmail.com>
2019-02-04 12:53:18 -0600

    black reformatted

--- socialSite/account/migrations/0003_auto_20190204_1830.py
+++ socialSite/account/migrations/0003_auto_20190204_1830.py
@@ -5,14 +5,14 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
-    dependencies = [
-        ('account', '0002_contact'),
-    ]
+    dependencies = [("account", "0002_contact")]
 
     operations = [
         migrations.AlterField(
-            model_name='profile',
-            name='photo',
-            field=models.ImageField(default='defaultUserImg.jpg', upload_to='users/%Y/%m/%d/'),
-        ),
+            model_name="profile",
+            name="photo",
+            field=models.ImageField(
+                default="defaultUserImg.jpg", upload_to="users/%Y/%m/%d/"
+            ),
+        )
     ]
