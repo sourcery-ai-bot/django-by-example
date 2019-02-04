6878e8d479c14182956f88e54994b93e2714717f
bjaus <brandon.jaus@gmail.com>
2019-02-03 22:57:13 -0600

    created form for image creation

---
+++ socialSite/images/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import Image
+
+
+class ImageCreateForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        fields = (
+            'title',
+            'url',
+            'description',
+        )
+        widgets = {
+            'url': forms.HiddenInput,
+        }
