da24fe4fa437ad65290a3aa0ea00ae18fc09da68
bjaus <brandon.jaus@gmail.com>
2019-02-03 22:15:03 -0600

    added authentication backends

--- socialSite/config/settings.py
+++ socialSite/config/settings.py
@@ -104,6 +104,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'account.authentication.EmailAuthBackend',
+]
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
