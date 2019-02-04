3e2d306c33f7aa67841bc0d1c6443254c633017e
bjaus <brandon.jaus@gmail.com>
2019-02-04 11:47:18 -0600

    created many-to-many relations between user and contact models

--- socialSite/account/models.py
+++ socialSite/account/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
@@ -27,3 +28,12 @@ class Contact(models.Model):
 
     def __str__(self):
         return f"{self.user_from} follows {self.user_to}"
+
+
+# Add following field to User dynamically
+User.add_to_class(
+    "following",
+    models.ManyToManyField(
+        "self", through=Contact, related_name="followers", symmetrical=False
+    ),
+)
