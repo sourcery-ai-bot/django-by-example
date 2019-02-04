f36a55562f48ae2646ddd5f09f6666bac4841413
bjaus <brandon.jaus@gmail.com>
2019-02-03 23:07:47 -0600

    added custom save method

--- socialSite/images/forms.py
+++ socialSite/images/forms.py
@@ -1,4 +1,7 @@
+from urllib import request
 from django import forms
+from django.core.files.base import ContentFile
+from django.util.text import slugify
 from .models import Image
 
 
@@ -22,3 +25,20 @@ class ImageCreateForm(forms.ModelForm):
             raise forms.ValidationError('The given URL does not '
                                         'match valid image extensions.')
         return url
+
+    def save(self, force_insert=False, force_update=False, commit=True):
+        image = super(ImageCreateForm, self).save(commit=False)
+        image_url = self.cleaned_data['url']
+        image_title = slugify(image.title)
+        extension = image_url.rsplit('.', 1)[1].lower()
+        image_name = f'{image_title}.{extension}'
+
+        # download image from the given URL
+        response = request.urlopen(image_url)
+        image.image.save(image_name,
+                         ContentFile(response.read()),
+                         save=False)
+
+        if commit:
+            image.save()
+        return image
