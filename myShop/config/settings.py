27160a8b8f4943840840abc123ea1a059af2b000
bjaus <brandon.jaus@gmail.com>
2019-02-04 22:17:59 -0600

    storing shopping carts in sessions

--- myShop/config/settings.py
+++ myShop/config/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django_extensions",
     # my apps
     "shop.apps.ShopConfig",
+    'cart.apps.CartConfig',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,5 @@ STATIC_URL = "/static/"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+
+CART_SESSION_ID = 'cart'
