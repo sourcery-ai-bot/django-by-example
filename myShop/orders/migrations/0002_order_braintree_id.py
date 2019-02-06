3f80bb71f78d4d990f926b75538cb2f4e6cd7122
bjaus <brandon.jaus@gmail.com>
2019-02-06 10:49:49 -0600

    integrating braintree using hosted fields

--- myShop/orders/migrations/0002_order_braintree_id.py
+++ myShop/orders/migrations/0002_order_braintree_id.py
@@ -5,14 +5,12 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
-    dependencies = [
-        ('orders', '0001_initial'),
-    ]
+    dependencies = [("orders", "0001_initial")]
 
     operations = [
         migrations.AddField(
-            model_name='order',
-            name='braintree_id',
+            model_name="order",
+            name="braintree_id",
             field=models.CharField(blank=True, max_length=50),
-        ),
+        )
     ]
