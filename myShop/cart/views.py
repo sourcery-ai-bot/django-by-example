bbe253121bc0158fc2237e7fee7f5b4733b8a4f7
bjaus <brandon.jaus@gmail.com>
2019-02-16 18:53:01 -0600

    adding products to the cart

--- myShop/cart/views.py
+++ myShop/cart/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404, redirect, render
-from django.views.decorator.http import require_POST
+from django.views.decorators.http import require_POST
 
 from shop.models import Product
 
