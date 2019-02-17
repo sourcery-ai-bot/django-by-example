b8daa7a20f04fc0b6edb598061c12855ad3bb5f8
bjaus <brandon.jaus@gmail.com>
2019-02-17 06:49:09 -0600

    adding custom actions to the admin site

--- myShop/orders/admin.py
+++ myShop/orders/admin.py
@@ -1,6 +1,42 @@
+import csv
+import datetime
+
 from django.contrib import admin
+from django.http import HttpResponse
 
 from .models import Order, OrderItem
+
+from ipdb import set_trace
+
+def export_to_csv(modeladmin, request, queryset):
+    set_trace()
+    opts = modeladmin.model._meta
+    response = HttpResponse(content_type="text/csv")
+    response[
+        "Content-Disposition"
+    ] = f"attachment;filename={opts.verbose_name}.csv"
+    writer = csv.writer(response)
+
+    fields = [
+        field
+        for field in opts.get_fields()
+        if not field.many_to_many and not field.one_to_many
+    ]
+    # Write a first row with header information
+    writer.writerow([field.verbose_name for field in fields])
+    # Write data rows
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime("%Y-%m-%d")
+            data_row.append(value)
+        writer.writerow(data_row)
+    return response
+
+
+export_to_csv.short_description = "Export to CSV"
 
 
 class OrderItemInline(admin.TabularInline):
@@ -24,3 +60,4 @@ class OrderAdmin(admin.ModelAdmin):
     ]
     list_filter = ["paid", "created", "updated"]
     inlines = [OrderItemInline]
+    actions = [export_to_csv]
